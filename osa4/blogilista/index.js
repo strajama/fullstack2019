e975982977da6cf2dc20eb935c4206eda27e2247
Susanna Rajamäki <strajama@gmail.com>
2019-02-14 19:11:50 +0200

    controller

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 
 const morgan = require('morgan')
@@ -28,24 +28,7 @@ app.use(morgan((tokens, req, res) => {
 
 app.use(cors())
 app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs.map(blog => blog.toJSON()))
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result.toJSON())
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
