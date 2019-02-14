d62b0cc093d4bbbb5dbf0a586480d16f1dd73229
Susanna Rajamäki <strajama@gmail.com>
2019-02-14 18:56:53 +0200

    mongoose omassa kansiossa ja kaikki toimii

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 const morgan = require('morgan')
 app.use(morgan('tiny', {
@@ -23,26 +23,6 @@ app.use(morgan((tokens, req, res) => {
 }, {
   skip: (request) => { return request.method !== 'POST' }
 }))
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI //'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
---
+++ osa4/blogilista/models/blog.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const mongoUrl = process.env.MONGODB_URI //'mongodb://localhost/bloglist'
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
